3db91dd8033f48e1574a9f6f066d9356f2990b14
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-10-22 14:21:31 +0530

    wrote test for areDimensionEqual

--- test/com/step/maths/MatrixTest.java
+++ test/com/step/maths/MatrixTest.java
@@ -1,6 +1,7 @@
 package com.step.maths;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -8,8 +9,16 @@ public class MatrixTest {
 
   @Test
   public void shouldReturnTextualFromOfMatrix() {
-    Matrix matrixA = new Matrix(0, 0);
-    System.out.println(matrix);
+    Matrix matrix = new Matrix(0, 0);
     assertEquals(matrix.toString(), "Matrix:\n");
+  }
+
+  @Test
+  public void shouldValidateIfDimensionsOfMatrixAreEqual() {
+    int[][] valuesA = { { 1, 2 }, { 3, 4 } };
+    int[][] valuesB = { { 2, 3 }, { 4, 5 } };
+    Matrix matrixA = Matrix.create(valuesA);
+    Matrix matrixB = Matrix.create(valuesB);
+    assertTrue(matrixA.areDimensionsEqual(matrixB));
   }
 }
