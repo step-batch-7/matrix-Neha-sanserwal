58c9ca6a3b3d3fde46792f4de375af8ee6bc5efc
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-10-22 14:47:57 +0530

    wrote aserts for areDimensionEqual

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
+rm -rf out/*
  javac -d out -cp ./libhamcrest-core-1.3.jar:./lib/junit-4.13.1.jar:.  $(find . -name "*.java") 
--- test/com/step/maths/MatrixTest.java
+++ test/com/step/maths/MatrixTest.java
@@ -1,6 +1,7 @@
 package com.step.maths;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,8 +18,12 @@ public class MatrixTest {
   public void shouldValidateIfDimensionsOfMatrixAreEqual() {
     int[][] valuesA = { { 1, 2 }, { 3, 4 } };
     int[][] valuesB = { { 2, 3 }, { 4, 5 } };
+    int[][] valuesC = { { 2 }, { 4 } };
+
     Matrix matrixA = Matrix.create(valuesA);
     Matrix matrixB = Matrix.create(valuesB);
+    Matrix matrixC = Matrix.create(valuesC);
     assertTrue(matrixA.areDimensionsEqual(matrixB));
+    assertFalse(matrixA.areDimensionsEqual(matrixC));
   }
 }
