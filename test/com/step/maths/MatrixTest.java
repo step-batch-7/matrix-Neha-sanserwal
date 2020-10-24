29c3aa963b64803d356b486295e46b281bbe116f
Neha Sanserwal <neha16sanserwal@gmail.com>
2020-10-24 11:07:54 +0530

    modified import of static mehtod

--- test/com/step/maths/MatrixTest.java
+++ test/com/step/maths/MatrixTest.java
@@ -1,9 +1,6 @@
 package com.step.maths;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
